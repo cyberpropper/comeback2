8bff66ddde1ea66a890af08c52ab92d5c40790b8
cyberpropper <osankin2002@mail.ru>
2024-09-04 21:25:34 +0700

    1

---
+++ src/main/java/com/i/comeback2/Comeback2Application.java
@@ -0,0 +1,13 @@
+package com.i.comeback2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comeback2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Comeback2Application.class, args);
+    }
+
+}
